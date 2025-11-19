babb46fd038047024d5137cf81a4b8f776dc5afa
Mohammad Baba <MOB010@diyarme.com>
2025-11-19 21:19:35 +0530

    Add project files.

---
+++ vs-2022-new-gitHub-experiance/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using vs_2022_new_gitHub_experiance.Models;
+
+namespace vs_2022_new_gitHub_experiance.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ vs-2022-new-gitHub-experiance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vs_2022_new_gitHub_experiance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
